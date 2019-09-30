e9adab4ab49a5757ae0c9ae18deee5ff8544750f
An-Cheng Yang <acyang0903@gmail.com>
2019-09-30 17:18:50 +0800

    add function pointer for caluating corelation

--- crop.py
+++ crop.py
@@ -14,8 +14,20 @@ import seaborn as sb
 import h5py
 import matplotlib.pyplot as plt
 
-#filename="/991GB/tmp/190407_CA014/000/channel_001.bin"
-#data = load_single_channel(filename, np.int16)
+def corl(s1, s2, time_window):
+    single1=pd.Series(s1)
+    single2=pd.Series(s2)
+    corrmat=np.zeros(shape=time_window, dtype=np.float64)
+    
+    for t in list(range(time_window)):
+        corrmat[t]=single1.corr(single2.shift(periods=-t), method="pearson")
+        print(t, corrmat[t])
+        
+    return corrmat
+
+def corl_dumy(s1, s2, time_window):
+    pass
+    return
 
 with h5py.File("output/7_sigma.h5", "r") as f:
     print(list(f.keys()))
@@ -23,16 +35,45 @@ with h5py.File("output/7_sigma.h5", "r") as f:
     index=f["index"][:]
     sd=f["Spike_Data"][:]
     
-#print(type(dset))
-print(index.shape, sd.shape)
+#print(index.shape, index.dtype)
+#print(sd.shape, sd.dtype)
+#print(sd[0,:])
 
-df = pd.DataFrame(sd) 
-#print(df)
+#nsingle=list(range(4))
+nsingle=list(range(sd.shape[0]))
+ntime=list(range(sd.shape[1]))
+flags=np.zeros(shape=sd.shape[0], dtype=np.int)
+#print(nsingle,ntime)
 
-corrmat = df.T.corr()
-mask=corrmat<0.9
-print(corrmat)
-print(mask) 
-  
-f, ax = plt.subplots(figsize =(9, 8)) 
-sb.heatmap(corrmat, ax = ax, cmap ="YlGnBu", mask = mask, linewidths = 0.01)   
+
+cal_corr=[corl, corl_dumy]
+
+#for i in nsingle:
+#    s1=pd.Series(sd[i,:])
+    
+#    for j in nsingle[i:]:
+#        s2=pd.Series(sd[j,:])
+
+#corrmat=np.zeros(shape=(sd.shape[0],sd.shape[0],sd.shape[1]), dtype=np.float64)
+#for i in nsingle:
+#    s1=pd.Series(sd[i,:])
+#    #print(s1)
+#    for j in nsingle[i:]:
+#        s2=pd.Series(sd[j,:])
+#        #print(i,j)
+#        for tau in ntime:
+#            #print(i, j, tau)
+#            #print(tau, s2.shift(periods=-tau))
+#            corrmat[i,j,tau]=s1.corr(s2.shift(periods=-tau), method="pearson")
+#    
+##print(corrmat[:,:,0])    
+##df = pd.DataFrame(sd) 
+##print(df)
+#
+##corrmat = df.T.corr()
+#mask=corrmat<0.9
+###print(corrmat)
+###print(mask) 
+#  
+#f, ax = plt.subplots(figsize =(9, 8)) 
+#sb.heatmap(corrmat[:,:,0], ax = ax, cmap ="YlGnBu", linewidths = 0.01)   
